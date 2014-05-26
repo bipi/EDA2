b528a476b3774b38925f08738bccb56b91a95c1b
bipi <bartosz.pyrz@gmail.com>
2014-05-26 15:37:07 +0200

    3/6

--- EDA2.R
+++ EDA2.R
@@ -10,38 +10,22 @@ plot(names(plot1),plot1, type='l')
 #2
 
 plot2<-tapply(NEI$Emissions[NEI$fips == "24510"], NEI$year[NEI$fips == "24510"], sum)
-plot(names(plot2),plot2, type='l') 
+plot(names(plot2),plot2, type='l')
 
 #3
-<<<<<<< HEAD
 plot3<-tapply(NEI$Emissions[NEI$fips == "24510"],
               list(NEI$year[NEI$fips == "24510"], NEI$type[NEI$fips == "24510"]),
               sum)
 plot3<-as.data.frame(plot3)
 names(plot3)<-c('nonroad','nonpoint','onroad','point')
-=======
-plot3<-tapply(NEI$Emissions[NEI$fips == "24510"], 
-              list(NEI$year[NEI$fips == "24510"], NEI$type[NEI$fips == "24510"]),
-              sum)
-plot3<-as.data.frame(plot3)
-names(plot3)<-c('nonroad','nonpoint','onroad','point') 
->>>>>>> origin/master
 plot(row.names(plot3),plot3$nonpoint, type='l')
 
 #4
 SCCm<-subset.data.frame(SCC, EI.Sector==('Fuel Comb - Electric Generation - Coal')
                         |EI.Sector==('Fuel Comb - Industrial Boilers, ICEs - Coal')
                         |EI.Sector==('Fuel Comb - Comm/Institutional - Coal')
-<<<<<<< HEAD
 )
 SCCmv<-SCCm$SCC
 NEIm<-subset.data.frame(NEI, SCC %in% SCCmv)
 plot4<-tapply(NEIm$Emissions, NEIm$year, sum)
 plot(names(plot4),plot4, type='l')
-=======
-                        )
-NEIm<-subset.data.frame(NEI, SCC==SCCmv)
-plot4<-tapply(NEIm$Emissions, NEIm$year, sum)
-plot(names(plot4),plot4, type='l')
-
->>>>>>> origin/master
